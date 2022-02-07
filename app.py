b8ce290be49b51acbf43116e5da4bca6d8e17858
sroy890 <67197217+sroy890@users.noreply.github.com>
2022-02-08 03:02:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,16 +20,16 @@ def predict():
 
     return render_template('index1.html', prediction_text='Expense = {}'.format(output))
 
-# @app.route('/predict',methods=['POST'])
-# def predict_api():
-#     '''
-#     For direct API calls trought request
-#     '''
-#     data = request.get_json(force=True)
-#     prediction = model.predict([np.array(list(data.values()))])
+@app.route('/predict',methods=['POST'])
+def predict_api():
+    '''
+    For direct API calls trought request
+    '''
+    data = request.get_json(force=True)
+    prediction = model.predict([np.array(list(data.values()))])
 
-#     output = prediction[0]
-#     return jsonify(output)
+    output = prediction[0]
+    return jsonify(output)
 
 if __name__ == "__main__":
     app.run(debug=True)
